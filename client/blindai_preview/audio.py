69533d8bb0dfd8758066fdbfdd248787cfd74599
kbamponsem <kwabenaamponsem@gmail.com>
2023-03-14 09:58:18 +0000

    Improves whisper audio API for the client

--- client/blindai_preview/audio.py
+++ client/blindai_preview/audio.py
@@ -2,6 +2,7 @@ import whisper
 from typing import Optional, Dict
 from .utils import torch_to_onnx
 from .client import BlindAiConnection
+from transformers import WhisperProcessor
 
 
 class Audio:
@@ -11,29 +12,62 @@ class Audio:
     def transcribe(
         cls,
         model: str,
-        file,
+        file: str,
+        transformer: str = "openai/whisper-tiny.en",
     ) -> Dict:
-
         """
-        - it retrieves the ONNX data associated with the model name
-        - it reads the file-like object with a sound library (e.g. soundfile) to get a numpy array
-        - it performs some Whisper-specific preprocessing on the client-side
-        - it calls “predict” to get an prediction object from the server
-        - it performs some post-processing to get a string and returns that string
+        BlindAI Whisper API which converts speech to text based on the model passed.
+
+        Args:
+            model: str
+                The Whisper model
+            file: str
+                Audio file to transcribe
+            transformer:
+                HuggingFace Whisper Transformer to use.
+        Returns:
+            Dict:
+                The transcription object containing, text and the tokens
         """
 
         # Get `model` from OpenAI's Whisper service
         torch_model = whisper.load_model(model)
 
         # Convert audio into numpy array
-        input_array = whisper.audio.load_audio(file)
+        audio = whisper.load_audio(file).flatten()
+
+        # Trim/pad audio to 30s with `pad_or_trim`
+        audio = whisper.pad_or_trim(audio)
+
+        # Convert loaded audio to log_mel_spectrogram
+        input_mel = whisper.log_mel_spectrogram(audio).unsqueeze(0)
 
         # Convert model to ONNX with `torch_to_onnx`
         onnx_file_path = torch_to_onnx(torch_model)
 
         # Upload ONNX model to BlindAI server
-        response = cls.blindai_connection.upload_model(onnx_file_path, model_name=model, optimize=False)
+        response = cls.blindai_connection.upload_model(
+            onnx_file_path, model_name=model, optimize=False
+        )
 
-        cls.blindai_connection.run_model(model_id=response.model_id, input_tensors=input_array)
+        # Run ONNX model with `input_array` on BlindAI server
+        res = cls.blindai_connection.run_model(
+            model_id=response.model_id, input_tensors=input_mel
+        )
 
-        return dict(model=onnx_file_path, array=input_array, model_id=response.model_id)
+        # Convert each output BlindAI Tensor object into PyTorch Tensor
+        res = [t.as_torch() for t in res.output]
+
+        # Load the Whisper Transformer object.
+        tokenizer = WhisperProcessor.from_pretrained(transformer)
+
+        # Extract tokens from result
+        tokens = res[0][0].numpy()
+
+        # Use transform to decode tokens
+        text = tokenizer.batch_decode(tokens, skip_special_tokens=True)
+
+        # Create transcription object
+        transcription = dict(text=text, tokens=tokens)
+
+        return transcription
--- client/blindai_preview/utils.py
+++ client/blindai_preview/utils.py
@@ -32,7 +32,7 @@ def torch_to_onnx(model: torch.nn.Module):
     # For the mean time, we will return a downloaded whisper model
 
     filename="fake-whisper-model.onnx"
-    os.system(f"gdown --id 1wqg1F0UkEdm3KB7n1BjfRLHnzKU2-G5S --output {filename}")
+    os.system(f"gdown 1wqg1F0UkEdm3KB7n1BjfRLHnzKU2-G5S --output {filename}")
 
     # Return the absolute path of the file
     return os.path.abspath(filename)
