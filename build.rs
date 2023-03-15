fc75ba9bbfb94aaddd6571395afa98974dd8c2af
JoFrost <mebessaa@student.42.fr>
2023-03-15 16:33:49 +0000

    fix: rename remote moddel upload flag

--- build.rs
+++ build.rs
@@ -1,8 +1,8 @@
 use std::env;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("cargo:rerun-if-env-changed=BLINDSPEECH_DISALLOW_UPLOAD");
-    let disallow_upload_remotely = env::var("BLINDSPEECH_DISALLOW_UPLOAD");
+    println!("cargo:rerun-if-env-changed=DISALLOW_REMOTE_UPLOAD");
+    let disallow_upload_remotely = env::var("DISALLOW_REMOTE_UPLOAD");
     match disallow_upload_remotely {
         Ok(_) => println!("cargo:rustc-cfg=DISALLOW_UPLOAD_REMOTELY=\"true\""),
         Err(_) => (),
